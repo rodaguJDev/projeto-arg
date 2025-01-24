45b8bae66a06c5087b74f5bf1d5e2ee2c80434dd
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-01-23 23:57:27 -0300

    fix: wave svg obbeys wave size

--- main.js
+++ main.js
@@ -4,20 +4,35 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const COUNT = 100;
+function loadWhiteDots() {
+    const COUNT = 100;
+    const MAX_MARGIN = window.innerWidth < 768 ? 15 : 80;
 
-const dotsLeft = document.querySelector('.dots-left');
-const dotsRight = document.querySelector('.dots-right');
-const elementPrefix = '<div class="white-dot" style="'
-const elementSufix = '"></div>'
+    const dotsLeft = document.querySelector(".dots-left");
+    const dotsRight = document.querySelector(".dots-right");
+    const elementPrefix = '<div class="white-dot" style="';
+    const elementSufix = '"></div>';
 
-let resultLeft = '';
-let resultRight = '';
+    let resultLeft = "";
+    let resultRight = "";
 
-for (let i = 0; i < COUNT; i++) {
-    resultLeft += elementPrefix + `margin-left: ${getRandomInt(1, 40)}px;margin-top: ${getRandomInt(50, 100)}px` + elementSufix
-    resultRight += elementPrefix + `margin-right: ${getRandomInt(1, 40)}px;margin-top: ${getRandomInt(50, 100)}px` + elementSufix
+    for (let i = 0; i < COUNT; i++) {
+        resultLeft +=
+            elementPrefix +
+            `margin-left: ${getRandomInt(1, MAX_MARGIN)}px;margin-top: ${getRandomInt(50, 100)}px` +
+            elementSufix;
+        resultRight +=
+            elementPrefix +
+            `margin-right: ${getRandomInt(1, MAX_MARGIN)}px;margin-top: ${getRandomInt(50, 100)}px` +
+            elementSufix;
+    }
+
+    if (dotsLeft) {
+        dotsLeft.innerHTML = resultLeft;
+    }
+    if (dotsRight) {
+        dotsRight.innerHTML = resultRight;
+    }
 }
 
-dotsLeft.innerHTML = resultLeft;
-dotsRight.innerHTML = resultRight;
+loadWhiteDots();
