3443f7c31b998cf9318a86a1fdf2312117cf251d
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-01-24 03:16:21 -0300

    feat: swiping

--- main.js
+++ main.js
@@ -39,6 +39,95 @@ function loadWhiteDots() {
     }
 }
 
+/**
+ *
+ * @param {HTMLDivElement} swipeObject
+ * @param {Function} onSwipeDone
+ * @returns {Object}
+ */
+function swipeListenerFactory(swipeObject) {
+    let startX = 0;
+    let currentX = 0;
+    let isSwiping = false;
+    const swipeThreshold = 60; // if swiped more than 60% of the way, finish swipe
+
+    function convertToPercentage(distanceInPixels) {
+        let movePercentage;
+        // rule of three: the width of the object is, by definition, 100% of the object's length
+        // so to convert pixels to percentage, you must use the object as a base
+        // multiply by -100 because we're swiping right to left and not left to right (if the value is negative, return a positive percentage)
+        movePercentage = (distanceInPixels / swipeObject.offsetWidth) * -100;
+
+        // clamp the values to be between 0 and 100% so the object can't go past the window
+        movePercentage = Math.max(0, Math.min(100, movePercentage));
+
+        return movePercentage;
+    }
+
+    function swipeStart(event) {
+        isSwiping = true;
+        startX = event.clientX || event.touches[0].clientX;
+        swipeObject.style.transition = "none"; // disable transition animations
+    }
+
+    function swipeUpdate(event) {
+        if (!isSwiping) return;
+
+        currentX = event.clientX || event.touches[0].clientX;
+        let swipeDistance = currentX - startX;
+        let percentage = convertToPercentage(swipeDistance);
+        console.log(percentage);
+
+        // the `right` parameter needs to be between -100 and 0, so subtract 100 from the movepercentage
+        swipeObject.style.right = `${percentage - 100}%`;
+    }
+
+    function swipeEnd(event) {
+        if (!isSwiping) return;
+
+        swipeObject.style.removeProperty("transition");
+
+        isSwiping = false;
+        let swipeDistance = currentX - startX;
+        let percentage = convertToPercentage(swipeDistance);
+
+        if (percentage > swipeThreshold) {
+            swipeObject.style.right = "0%";
+            removeListeners();
+        } else {
+            swipeObject.style.right = "-100%";
+        }
+    }
+
+    function removeListeners() {
+        document.removeEventListener("mousedown", swipeStart);
+        document.removeEventListener("mousemove", swipeUpdate);
+        document.removeEventListener("mouseup", swipeEnd);
+
+        // mobile
+        document.removeEventListener("touchstart", swipeStart);
+        document.removeEventListener("touchmove", swipeUpdate);
+        document.removeEventListener("touchend", swipeEnd);
+    }
+
+    function loadListeners() {
+        // pc
+        document.addEventListener("mousedown", swipeStart);
+        document.addEventListener("mousemove", swipeUpdate);
+        document.addEventListener("mouseup", swipeEnd);
+
+        // mobile
+        document.addEventListener("touchstart", swipeStart);
+        document.addEventListener("touchmove", swipeUpdate);
+        document.addEventListener("touchend", swipeEnd);
+    }
+
+    return {
+        loadListeners,
+        removeListeners,
+    };
+}
+
 function swipe() {
     /**
      * @type {HTMLDivElement|null}
@@ -50,7 +139,15 @@ function swipe() {
 }
 
 function load() {
+    // render
     loadWhiteDots();
+
+    // load swiper
+    const swipeObject = document.querySelector(".swiper");
+    const swipeListener = swipeListenerFactory(swipeObject);
+    swipeListener.loadListeners();
 }
 
 load();
+
+swipeObject = document.querySelector(".swiper");
