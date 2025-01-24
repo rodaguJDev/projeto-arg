e441996426214db0408c29df78a8a26c27d0121e
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-01-24 00:34:25 -0300

    fix: white dots generation distance measured in percentage instead of pixel

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
+
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomDouble(min, max, precision = 1) {
+    const precisionFactor = Math.pow(10, precision);
+    return getRandomInt(0, max * precisionFactor) / precisionFactor + min;
 }
 
 function loadWhiteDots() {
-    const COUNT = 100;
-    const MAX_MARGIN = window.innerWidth < 768 ? 15 : 80;
+    const DOT_COUNT = 100;
 
     const dotsLeft = document.querySelector(".dots-left");
     const dotsRight = document.querySelector(".dots-right");
@@ -16,14 +20,14 @@ function loadWhiteDots() {
     let resultLeft = "";
     let resultRight = "";
 
-    for (let i = 0; i < COUNT; i++) {
+    for (let i = 0; i < DOT_COUNT; i++) {
         resultLeft +=
             elementPrefix +
-            `margin-left: ${getRandomInt(1, MAX_MARGIN)}px;margin-top: ${getRandomInt(50, 100)}px` +
+            `margin-left: ${getRandomDouble(1, 100)}%;margin-top: ${getRandomDouble(50, 100)}px` +
             elementSufix;
         resultRight +=
             elementPrefix +
-            `margin-right: ${getRandomInt(1, MAX_MARGIN)}px;margin-top: ${getRandomInt(50, 100)}px` +
+            `margin-right: ${getRandomDouble(1, 100)}%;margin-top: ${getRandomDouble(50, 100)}px` +
             elementSufix;
     }
 
