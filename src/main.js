5b0c3bc35816d7da6d757ad22bd81b8cee7d3cdb
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-09 20:41:39 -0300

    build: use solidjs framework

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import "./css/app.css";
-import { setupCounter } from "./js/counter.js";
-import app from "./pages/index.js";
-
-document.querySelector("#app").innerHTML = app;
-
-setupCounter(document.querySelector("#counter"));
