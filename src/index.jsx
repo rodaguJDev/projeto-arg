9b2bb3f6592153386031d3a855ddfd5bd4905f44
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-09 23:48:09 -0300

    feat: both waves with dotted falls

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 /* @refresh reload */
-import { render } from "solid-js/web";
+import { render } from 'solid-js/web';
 
-import "./css/app.css";
-import App from "./components/App";
+import './css/app.css';
+import App from './pages/App';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
-  );
+    throw new Error(
+        'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?'
+    );
 }
 
 render(() => <App />, root);
