a9065d8d5e440d9abf5ce053ac97be667a42df4c
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-10 01:58:39 -0300

    add github pages deploy dep

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/projeto-arg/',
     plugins: [tailwindcss(), solidPlugin()],
     server: {
         port: 3000,
     },
     build: {
-        target: "esnext",
+        target: 'esnext',
     },
 });
