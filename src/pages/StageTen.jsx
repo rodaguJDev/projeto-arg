9ad1e0bf5b6d671a885ced944e74d86a0cc7e37a
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-12 22:53:19 -0300

    feat: stage 10 finished

--- src/pages/StageTen.jsx
+++ src/pages/StageTen.jsx
@@ -1,12 +1,29 @@
-import asset from '../assets/images/4/1.jpeg';
-import AnswerBox from '../components/AnswerBox';
+import asset from '../assets/images/10/1.png';
+import TypeWritter from '../js/typewritter';
 import StagesLayout from '../layouts/StagesLayout';
 
+async function start() {
+    const writter = new TypeWritter(200);
+    const lines = document.querySelectorAll('.js-write');
+
+    writter.writeLines(lines);
+}
+
 function StageTen() {
+    setTimeout(start, 2000);
     return (
-        <StagesLayout fadeInDelay={3000}>
-            <div class="w-fit space-y-2">
+        <StagesLayout fadeInDelay={1500}>
+            <div class="w-fit space-y-2 text-center">
                 <img class="pointer-events-none w-128" src={asset} />
+                <span
+                    className="js-write mt-12 block text-3xl font-medium text-white"
+                    data-content="ELE VAI VOLTAR..."
+                ></span>
+
+                <span
+                    className="js-write mt-2 block text-xl text-white"
+                    data-content="ENIGMA #2 RESOLVIDO"
+                ></span>
             </div>
         </StagesLayout>
     );
